e16f0596e2b0cf1eef282de6b590ea059836d44b
Patrik Frey <p.frey92@hotmail.com>
2016-04-17 15:54:31 +0200

    added and updated ppm_reader_simple, servo_test_simple, ESC_out_test_..

--- test/ESC_out_test_native_servo/ESC_out_test_native_servo.ino
+++ test/ESC_out_test_native_servo/ESC_out_test_native_servo.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+//Testing ESC
+// do not connect 5V to Arduino (only GND and Signal)
+//only connect ESC to power AFTER Arduino is powered up
 
+#include <Servo.h>
+
+Servo myESC;  // create servo object to control ESC
+String text;
+int value;
+
+void setup() 
+{
+  Serial.begin(9600);
+  myESC.attach(9);  // attaches the servo on pin 9 to the servo object
+  myESC.writeMicroseconds(1000); //write lowest value to ESC, to avoid it going into calibration or programming mode and to arm it
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  while(Serial.available())
+  {
+    text=Serial.readString();
+    value=text.toInt(); // =0 if string is not a number
+    Serial.print("received: ");
+    Serial.print(value);
+    Serial.print("\n");
+
+    //Change PWM output
+    myESC.writeMicroseconds(value);
+  }
 
 }
